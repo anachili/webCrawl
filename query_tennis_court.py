61f619cce2ee27e687da8f81073fcd5f529ba7cd
Anna Chi <hengchi@amazon.com>
2023-10-07 16:43:54 +0100

    Fixed doc select bugs + added args + merged time slots

--- query_tennis_court.py
+++ query_tennis_court.py
@@ -6,7 +6,14 @@ from bs4 import BeautifulSoup
 from selenium.common.exceptions import TimeoutException
 from datetime import datetime, timedelta
 import sys
+import argparse
+import re
 
+TIME_FORMAT = '%H:%M'
+WORK_DAY_NO_EARILER_THAN = 8
+WORK_DAY_NO_LATER_THAN = 10
+WORK_NIGHT_NO_EARILER_THAN = 18
+WEEKEND_NO_EARILER_THAN = 9
 
 def generate_dates(start_date, end_date):
     """
@@ -34,22 +41,17 @@ def generate_dates(start_date, end_date):
     return date_strings
 
 # Function to extract session info
-def extract_session_info(session):
-    time_span = session.find('span', class_='available-booking-slot')
-    name_span = session.find('span', class_='cost')
-
-    # Return None for any missing value
+def extract_session_info(time_span):
     session_time = time_span.text if time_span and time_span.text else "NA"
-    session_name = name_span.text if name_span and name_span.text else "NA"
-    # print("TTTT")
-    # print(time_span)
-    # print(time_span.text)
-    # print(name_span)
-    # print(name_span.text)
+    return session_time
 
-    return session_time, session_name
+def convert_time_string_to_number(time_string: str) -> int:
+    # Parse the string to a datetime object
+    time_object = datetime.strptime(time_string, "%H:%M")
+    # Convert to decimal number
+    return time_object.hour + time_object.minute/60.0
 
-def parse_time(input_string: str):
+def parse_time(input_string: str) -> int:
     try:
         # Split the string by '-' to separate the time ranges
         time_ranges = input_string.split('-')
@@ -59,40 +61,85 @@ def parse_time(input_string: str):
         end_time_str = time_ranges[1].strip()
 
         # Convert the start and end times to datetime objects
-        time_format = '%H:%M'
-        start_time = datetime.strptime(start_time_str, time_format)
-        end_time = datetime.strptime(end_time_str, time_format)
+        TIME_FORMAT = '%H:%M'
+        start_time = convert_time_string_to_number(start_time_str)
+        end_time = convert_time_string_to_number(end_time_str)
         return start_time, end_time
 
     except ValueError:
         print(f"Time data '{input_string}' does not match format '%H:%M'")
 
-def is_weekend_friendly(input_string: str):
-    start_time, end_time = parse_time(input_string)
-    time_format = '%H:%M'
+def is_weekend_friendly(start_time, end_time):
+    
 
-    if start_time <= datetime.strptime('12:00', time_format):
+    if start_time <= WEEKEND_NO_EARILER_THAN:
         return False
     return True
 
 
-def is_workday_friendly(input_string: str):
-    start_time, end_time = parse_time(input_string)
-    time_format = '%H:%M'
+def is_workday_friendly(start_time, end_time):
+    TIME_FORMAT = '%H:%M'
+    at_eighteen = datetime.strptime('18:00', TIME_FORMAT)
 
-    # Now, you can compare these datetime objects with the specified target times (10:00 and 18:00)
-    target_time1 = datetime.strptime('8:00', time_format)
-    target_time2 = datetime.strptime('10:00', time_format)
-    
-    target_time3 = datetime.strptime('18:00', time_format)
-    # target_time4 = datetime.strptime('18:00', time_format)
-
-    if end_time <= target_time2 and start_time >= target_time1:
+    if end_time <= WORK_DAY_NO_LATER_THAN and start_time >= WORK_DAY_NO_EARILER_THAN:
         return True
-    if start_time >= target_time3:
+    if start_time >= WORK_NIGHT_NO_EARILER_THAN:
         return True
     else:
         return False
+
+def print_debug(msg):
+    print(f'DEBUG: {msg}')
+
+def merge_time_slots(slots):
+    # Sort the slots
+    slots.sort()
+    
+    merged_slots = []
+    current_start, current_end = slots[0]
+    
+    for start, end in slots[1:]:
+        if start <= current_end:  # Overlapping
+            current_end = max(current_end, end)  # Extend the current interval
+        else:  # Non-overlapping
+            merged_slots.append((current_start, current_end))
+            current_start, current_end = start, end
+    
+    # Save the last interval
+    merged_slots.append((current_start, current_end))
+    
+    return merged_slots
+
+# def merge_time_slots(slots):
+#     print_debug(f'SSSSS: {slots}')
+#     # Extract time intervals and sort them
+#     time_intervals = sorted([
+#         (
+#             datetime.strptime(re.search(r"(\d{2}:\d{2})", slot).group(), TIME_FORMAT),
+#             datetime.strptime(re.search(r"- (\d{2}:\d{2})", slot).group(1), TIME_FORMAT)
+#         )
+#         for slot in slots
+#     ])
+    
+#     merged_intervals = []
+#     # Initialize with the first interval
+#     current_start, current_end = time_intervals[0]
+    
+#     for start, end in time_intervals[1:]:
+#         if start <= current_end:  # Overlapping
+#             current_end = max(current_end, end)  # Extend the current interval
+#         else:  # Non-overlapping
+#             # Save the previous interval and move to the next
+#             merged_intervals.append((current_start, current_end))
+#             current_start, current_end = start, end
+    
+#     # Save the last interval
+#     merged_intervals.append((current_start, current_end))
+    
+#     # Convert intervals to desired string format
+#     merged_slots = [f"Book at {start.strftime(TIME_FORMAT)} - {end.strftime(TIME_FORMAT)}" for start, end in merged_intervals]
+    
+#     return merged_slots
 
 
 def find_slots(driver, date: str, place: str, is_weekend = True):
@@ -115,44 +162,54 @@ def find_slots(driver, date: str, place: str, is_weekend = True):
         soup = BeautifulSoup(html_content, 'lxml')
 
         # Find all the resource sessions
-        resource_sessions = soup.find_all('div', class_='resource-session')
+        resource_sessions = soup.find_all('span', class_='available-booking-slot')
 
         # Extract the session names and times
-        res = []
+        slots = []
         for session in resource_sessions:
             item = extract_session_info(session)
-            if item[0] != "NA":
-                if is_weekend:
-                    if is_weekend_friendly(item[0]):
-                        res.append(item)
-                elif is_workday_friendly(item[0]):
-                    res.append(item)
-            # print(item)
-            # break
 
+            if item and item != "NA":
+                start_time, end_time = parse_time(item)
+
+                if is_weekend:
+                    if is_weekend_friendly(start_time, end_time):
+                        slots.append((start_time, end_time))
+                elif is_workday_friendly(start_time, end_time):
+                    slots.append((start_time, end_time))
         print(place)
         print(date)
-        print(res)
+
+        if len(slots) > 1:
+            merged_slots = merge_time_slots(slots)
+            print(merged_slots)
+        else:
+            print(slots)
 
 # Set up the driver (assuming Chrome and chromedriver in this example)
 driver = webdriver.Chrome()  # Update the path
 
-if len(sys.argv) > 1:  # Check if arguments are provided
-    for i, arg in enumerate(sys.argv[1:], 1):
-        if i == 1:
-            dates = [arg]
-        if i == 2:
-            places = [arg]
-else:
-    dates = generate_dates('2023-09-30', '2023-09-30')
-
-workday_places = ['MillfieldsParkMiddlesex', 'SouthwarkPark', 'PoplarRecGround', 'BethnalGreenGardens', 'RopemakersFieldLONDON', 'KingEdwardMemorialPark', 'StJohnsParkLondon', 'VictoriaParkLONDON', 'WappingGardens', 'LondonFieldsPark', 'SpringHillParkTennis', 'HackneyDowns', 'ClissoldParkHackney', 'BrunswickPark', 'BurgessParkSouthwark', 'TannerStPark']
+workday_places = ['MillfieldsParkMiddlesex', 'SouthwarkPark', 'BethnalGreenGardens', 'RopemakersFieldLONDON', 'KingEdwardMemorialPark', 'VictoriaParkLONDON', 'WappingGardens', 'LondonFieldsPark', 'HackneyDowns', 'ClissoldParkHackney', 'BurgessParkSouthwark', 'TannerStPark']
 weekend_places = ['SouthwarkPark', 'PoplarRecGround', 'BethnalGreenGardens', 'RopemakersFieldLONDON', 'KingEdwardMemorialPark', 'StJohnsParkLondon', 'VictoriaParkLONDON', 'WappingGardens', 'LondonFieldsPark', 'BrunswickPark', 'BurgessParkSouthwark', 'TannerStPark']
 
-places = weekend_places
+
+parser = argparse.ArgumentParser(description='A simple argparse example.')
+parser.add_argument('--places', nargs='*')
+parser.add_argument('--start-date')
+parser.add_argument('--end-date')
+parser.add_argument('--is-weekend')
+
+args = parser.parse_args()
+
+is_weekend = True if args.is_weekend == 'true' else False
+places = args.places if args.places else (weekend_places if is_weekend else workday_places)
+start_date = args.start_date
+end_date = args.end_date
+
+dates = generate_dates(start_date, end_date)
+print(f'Is weekend?: {is_weekend}')
 print(dates)
 print(places)
-is_weekend = True
 
 for date in dates:
     for place in places:
